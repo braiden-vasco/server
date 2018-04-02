36fd53dcd9aadd0d9c9d31f3afa1482ecf3bb4a5
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-04-02 19:43:54 +0000

    Rename file

--- src/vpn.rb
+++ src/vpn.rb
@@ -9,7 +9,7 @@ system! 'adduser --system --group --no-create-home openvpn'
 system! 'apt-get install --yes openvpn'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/vpn/openvpn.conf'),
+        File.expand_path('../files/vpn/server.conf'),
         '/etc/openvpn/server.conf'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
