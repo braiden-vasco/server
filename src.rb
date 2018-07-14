eea7233c0de8d772bd00c8ef6a96bd338cc02536
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-07-14 07:12:18 +0000

    Fix paths

--- src.rb
+++ src.rb
@@ -15,11 +15,11 @@ def system!(*args)
 end
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/apt/sources.list', __dir__),
+        File.expand_path('files/apt/sources.list', __dir__),
         '/etc/apt/sources.list'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/apt/02noinstall', __dir__),
+        File.expand_path('files/apt/02noinstall', __dir__),
         '/etc/apt/apt.conf.d/02noinstall'
 
 system! 'apt-get update'
@@ -30,23 +30,23 @@ system! 'apt-get autoremove --purge --yes'
 system! 'apt-get install --yes unattended-upgrades'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/apt/20auto-upgrades', __dir__),
+        File.expand_path('files/apt/20auto-upgrades', __dir__),
         '/etc/apt/apt.conf.d/20auto-upgrades'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/hostname', __dir__),
+        File.expand_path('files/hostname', __dir__),
         '/etc/hostname'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/hosts', __dir__),
+        File.expand_path('files/hosts', __dir__),
         '/etc/hosts'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/aliases', __dir__),
+        File.expand_path('files/aliases', __dir__),
         '/etc/aliases'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/sysctl.conf', __dir__),
+        File.expand_path('files/sysctl.conf', __dir__),
         '/etc/sysctl.d/00-sysctl.conf'
 
 system! 'sysctl -p /etc/sysctl.d/00-sysctl.conf'
@@ -54,11 +54,11 @@ system! 'sysctl -p /etc/sysctl.d/00-sysctl.conf'
 system! 'apt-get install --yes iptables-persistent'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/iptables/rules.v4'),
+        File.expand_path('files/iptables/rules.v4'),
         '/etc/iptables/rules.v4'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/iptables/rules.v6'),
+        File.expand_path('files/iptables/rules.v6'),
         '/etc/iptables/rules.v6'
 
 system! 'cat /etc/iptables/rules.v4 | iptables-restore'
@@ -67,31 +67,31 @@ system! 'cat /etc/iptables/rules.v6 | ip6tables-restore'
 system! 'apt-get install --yes sudo'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '440',
-        File.expand_path('../files/sudoers', __dir__),
+        File.expand_path('files/sudoers', __dir__),
         '/etc/sudoers'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssh/sshd_config'),
+        File.expand_path('files/ssh/sshd_config'),
         '/etc/ssh/sshd_config'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssh/ssh_host_dsa_key.pub'),
+        File.expand_path('files/ssh/ssh_host_dsa_key.pub'),
         '/etc/ssh/ssh_host_dsa_key.pub'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssh/ssh_host_ecdsa_key.pub'),
+        File.expand_path('files/ssh/ssh_host_ecdsa_key.pub'),
         '/etc/ssh/ssh_host_ecdsa_key.pub'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssh/ssh_host_ed25519_key.pub'),
+        File.expand_path('files/ssh/ssh_host_ed25519_key.pub'),
         '/etc/ssh/ssh_host_ed25519_key.pub'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssh/ssh_host_key.pub'),
+        File.expand_path('files/ssh/ssh_host_key.pub'),
         '/etc/ssh/ssh_host_key.pub'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssh/ssh_host_rsa_key.pub'),
+        File.expand_path('files/ssh/ssh_host_rsa_key.pub'),
         '/etc/ssh/ssh_host_rsa_key.pub'
 
 system! "id braiden-vasco || adduser --disabled-password --gecos '' braiden-vasco"
@@ -103,7 +103,7 @@ system! 'install', '-o', 'braiden-vasco', '-g', 'braiden-vasco', '-m', '700',
         '-d', '/home/braiden-vasco/.ssh'
 
 system! 'install', '-o', 'braiden-vasco', '-g', 'braiden-vasco', '-m', '644',
-        File.expand_path('../files/user/authorized_keys'),
+        File.expand_path('files/user/authorized_keys'),
         '/home/braiden-vasco/.ssh/authorized_keys'
 
 system! 'install', '-o', 'braiden-vasco', '-g', 'braiden-vasco', '-m', '755',
@@ -122,13 +122,13 @@ system! 'apt-get install --yes vim'
 system! 'update-alternatives --set editor /usr/bin/vim.basic'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/vim/vimrc.local'),
+        File.expand_path('files/vim/vimrc.local'),
         '/etc/vim/vimrc.local'
 
 system! 'apt-get install --yes tor'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/tor/torrc', __dir__),
+        File.expand_path('files/tor/torrc', __dir__),
         '/etc/tor/torrc'
 
 system! 'install', '-o', 'debian-tor', '-g', 'debian-tor', '-m', '700',
@@ -138,38 +138,38 @@ system! 'install', '-o', 'debian-tor', '-g', 'debian-tor', '-m', '700',
         '-d', '/var/lib/tor/hidden_services/random'
 
 system! 'install', '-o', 'debian-tor', '-g', 'debian-tor', '-m', '600',
-        File.expand_path('../files/tor/hostname'),
+        File.expand_path('files/tor/hostname'),
         '/var/lib/tor/hidden_services/random/hostname'
 
 system! 'adduser --system --group --no-create-home openvpn'
 system! 'apt-get install --yes openvpn'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/vpn/server.conf'),
+        File.expand_path('files/vpn/server.conf'),
         '/etc/openvpn/server.conf'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/vpn/ca.crt'),
+        File.expand_path('files/vpn/ca.crt'),
         '/etc/openvpn/ca.crt'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/vpn/server.crt'),
+        File.expand_path('files/vpn/server.crt'),
         '/etc/openvpn/server.crt'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/vpn/dh2048.pem'),
+        File.expand_path('files/vpn/dh2048.pem'),
         '/etc/openvpn/dh2048.pem'
 
 system! 'systemctl daemon-reload'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/resolv.conf', __dir__),
+        File.expand_path('files/resolv.conf', __dir__),
         '/etc/resolv.conf'
 
 system! 'apt-get install --yes dnsmasq'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/dnsmasq.conf', __dir__),
+        File.expand_path('files/dnsmasq.conf', __dir__),
         '/etc/dnsmasq.d/00-dnsmasq.conf'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '755',
@@ -182,17 +182,17 @@ system! 'install', '-o', 'root', '-g', 'root', '-m', '700',
         '-d', '/etc/ssl/private'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ssl/braiden-vasco_network.crt-bundle'),
+        File.expand_path('files/ssl/braiden-vasco_network.crt-bundle'),
         '/etc/ssl/public/braiden-vasco_network.crt-bundle'
 
 system! 'apt-get install --yes nginx'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/www/index.html', __dir__),
+        File.expand_path('files/www/index.html', __dir__),
         '/var/www/html/index.html'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/www/pgp-public-key.asc'),
+        File.expand_path('files/www/pgp-public-key.asc'),
         '/var/www/html/pgp-public-key.asc'
 
 system! 'wget', EMERCOIN_URL, '-O', '/tmp/emercoin.tar.gz'
@@ -227,27 +227,27 @@ system! 'install -o root -g root -m 755 /tmp/namecoind /usr/local/bin'
 system! 'install -o root -g root -m 755 /tmp/ncdns     /usr/local/bin'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/emercoin/emercoin.conf', __dir__),
+        File.expand_path('files/emercoin/emercoin.conf', __dir__),
         '/etc/emercoin/emercoin.conf'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/namecoin/namecoin.conf', __dir__),
+        File.expand_path('files/namecoin/namecoin.conf', __dir__),
         '/etc/namecoin/namecoin.conf'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ncdns/ncdns.conf', __dir__),
+        File.expand_path('files/ncdns/ncdns.conf', __dir__),
         '/etc/ncdns/ncdns.conf'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/emercoin/emercoin.service', __dir__),
+        File.expand_path('files/emercoin/emercoin.service', __dir__),
         '/etc/systemd/system/emercoin.service'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/namecoin/namecoin.service', __dir__),
+        File.expand_path('files/namecoin/namecoin.service', __dir__),
         '/etc/systemd/system/namecoin.service'
 
 system! 'install', '-o', 'root', '-g', 'root', '-m', '644',
-        File.expand_path('../files/ncdns/ncdns.service', __dir__),
+        File.expand_path('files/ncdns/ncdns.service', __dir__),
         '/etc/systemd/system/ncdns.service'
 
 system! 'systemctl daemon-reload'
